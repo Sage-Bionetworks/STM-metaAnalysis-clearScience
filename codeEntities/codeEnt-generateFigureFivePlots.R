1bb6e7a2fbc50a753da722f7bd192ccfc9f194be
erichhuang <erich@me.com>
2013-04-04 22:39:17 +0000

    Code entity for generating Figure 5 plots.

---
+++ codeEntities/codeEnt-generateFigureFivePlots.R
@@ -0,0 +1,11 @@
+## prepareFigureFiveData.R
+
+## Erich S. Huang
+## Sage Bionetworks
+## erich.huang@sagebase.org
+
+## REQUIRE
+require(rGithubClient)
+
+## SOURCE FUNCTION
+sourceRepoFile('Sage-Bionetworks/STM-metaAnalysis-clearScience', 'functions/cciByClinicalCovariates.R')
