1a10f54164f1d61ce71ce552ed2917263cbd20bb
erichhuang <erich@me.com>
2013-04-04 22:47:01 +0000

    Beginning work on regenerating Figure 4 panels.

---
+++ functions/figureFour.R
@@ -0,0 +1,22 @@
+## figureFour.R
+
+## Erich S. Huang
+## Sage Bionetworks
+## erich.huang@sagebase.org
+
+## Adapted from Erhan Biilal's Figure 4 work on
+## analyzing the results of the BCC challenge.
+
+figureFour <- function(){
+  ## REQUIRE
+  require(synapseClient)
+  require(ggplot2)
+  require(reshape)
+  require(gtools)
+  
+  
+  
+  
+  
+  
+}
