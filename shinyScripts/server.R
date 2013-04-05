63a72bc6f2a0879423c635d6ebb03a19856a2266
erichhuang <erich@mac.com>
2013-04-05 22:04:23 +0000

    Changed figure title.

--- shinyScripts/server.R
+++ shinyScripts/server.R
@@ -100,7 +100,7 @@ shinyServer(function(input, output) {
     lbBoxplot <- ggplot(boxplotDF, aes(x = factor(category), y = scores)) + 
       geom_boxplot() + 
       geom_jitter(aes(colour = factor(category)), size = 4) +
-      ggtitle('All Patients versus Clinical Subgroup\n') +
+      ggtitle('All Patients versus Selected Subgroup\n') +
       ylab('Concordance Index\n') + xlab('\nCategory')
     show(lbBoxplot)
   })
