faedd1689418d6cd926872dc714f852941d3e04e
Erich S. Huang <erich@mac.com>
2013-04-05 22:47:50 +0000

    Code efficiencies.

--- shinyScripts/server.R
+++ shinyScripts/server.R
@@ -30,26 +30,7 @@ subgroupLbGenerator <- function(subgroupVector){
 }
 
 # pre-define sub-grouped leaderboards
-erPosLB <- subgroupLbGenerator(sGroupList$erPos)
-erNegLB <- subgroupLbGenerator(sGroupList$erNeg)
-her2PosLB <- subgroupLbGenerator(sGroupList$her2Pos)
-her2NegLB <- subgroupLbGenerator(sGroupList$her2Neg)
-prPosLB <- subgroupLbGenerator(sGroupList$prPos)
-prNegLB <- subgroupLbGenerator(sGroupList$prNeg)
-grade1LB <- subgroupLbGenerator(sGroupList$grade1)
-grade2LB <- subgroupLbGenerator(sGroupList$grade2)
-grade3LB <- subgroupLbGenerator(sGroupList$grade3)
-lnNegLB <- subgroupLbGenerator(sGroupList$lnNeg)
-lnN1LB <- subgroupLbGenerator(sGroupList$ln1to3)
-lnN2LB <- subgroupLbGenerator(sGroupList$ln4to9)
-lnN3LB <- subgroupLbGenerator(sGroupList$ln10plus)
-timeALB <- subgroupLbGenerator(sGroupList$time0to5)
-timeBLB <- subgroupLbGenerator(sGroupList$time5to10)
-timeCLB <- subgroupLbGenerator(sGroupList$time10plus)
-ageALB <- subgroupLbGenerator(sGroupList$preMeno)
-ageBLB <- subgroupLbGenerator(sGroupList$postMeno)
-sizeALB <- subgroupLbGenerator(sGroupList$size0to2)
-sizeBLB <- subgroupLbGenerator(sGroupList$size2plus)
+subgroupDFList <- lapply(sGroupList, subgroupLbGenerator)
 
 # set up a dataframe for boxplots
 boxplotDF <- data.frame('category' = rep('allPatients', nrow(oslovalLB)), 
@@ -62,26 +43,26 @@ shinyServer(function(input, output) {
   datasetInput <- reactive({
     switch(input$clinCovar,
            "All Patients" = oslovalLB,
-           "ER Positive" = erPosLB,
-           "ER Negative" = erNegLB,
-           'HER2 Positive' = her2PosLB,
-           'HER2 Negative' = her2NegLB,
-           'PR Positive' = prPosLB,
-           'PR Negative' = prNegLB,
-           'Histologic Grade 1' = grade1LB,
-           'Histologic Grade 2' = grade2LB,
-           'Histologic Grade 3' = grade3LB,
-           'Lymph Node Negative' = lnNegLB,
-           'Lymph Node 1-3' = lnN1LB,
-           'Lymph Node 4-9' = lnN2LB,
-           'Lymph Node 10+' = lnN3LB,
-           'Followup Time 0-5 Years' = timeALB,
-           'Followup Time 5-10 Years' = timeBLB,
-           'Followup Time 10+ Years' = timeCLB,
-           'Age ≤ 50 Years' = ageALB,
-           'Age 50+ Years' = ageBLB,
-           'Tumor Size 0-2 CM' = sizeALB,
-           'Tumor Size 2+ CM' = sizeBLB)
+           "ER Positive" = subgroupDFList$erPos,
+           "ER Negative" = subgroupDFList$erNeg,
+           'HER2 Positive' = subgroupDFList$her2Pos,
+           'HER2 Negative' = subgroupDFList$her2Neg,
+           'PR Positive' = subgroupDFList$prPos,
+           'PR Negative' = subgroupDFList$prNeg,
+           'Histologic Grade 1' = subgroupDFList$grade1,
+           'Histologic Grade 2' = subgroupDFList$grade2,
+           'Histologic Grade 3' = subgroupDFList$grade3,
+           'Lymph Node Negative' = subgroupDFList$lnNeg,
+           'Lymph Node 1-3' = subgroupDFList$ln1to3,
+           'Lymph Node 4-9' = subgroupDFList$ln4to9,
+           'Lymph Node 10+' = subgroupDFList$ln10plus,
+           'Followup Time 0-5 Years' = subgroupDFList$time0to5,
+           'Followup Time 5-10 Years' = subgroupDFList$time5to10,
+           'Followup Time 10+ Years' = subgroupDFList$time10plus,
+           'Age ≤ 50 Years' = subgroupDFList$preMeno,
+           'Age 50+ Years' = subgroupDFList$postMeno,
+           'Tumor Size 0-2 CM' = subgroupDFList$size0to2,
+           'Tumor Size 2+ CM' = subgroupDFList$size2plus)
   })
   
   # Server outputs
