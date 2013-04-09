c3cd2448813df8b2e49de391dda5f8df30ad5299
erichhuang <erich@me.com>
2013-04-09 16:40:12 +0000

    Breaking out Figure Four functions into separate panels.

---
+++ functions/figureFourA.R
@@ -0,0 +1,64 @@
+## figureFourA.R
+
+## Erich S. Huang
+## Sage Bionetworks
+## erich.huang@sagebase.org
+
+## Adapted from Erhan Biilal's Figure 4A work on
+## analyzing the results of the BCC challenge.
+
+figureFourA <- function(){
+  ## REQUIRE
+  require(synapseClient)
+  require(ggplot2)
+  require(reshape)
+  require(gtools)
+  require(survcomp)
+  
+  ## LOAD NECESSARY DATA OBJECTS
+  metbEnt <- loadEntity('syn1738796') # matrix of predictions
+  
+  metbSurvEnt <- loadEntity('syn1125632') # survival object
+  
+  metbLbEnt <- loadEntity('syn1744693') # 15 October leaderboard
+  
+  metbPredMat <- metbEnt$objects$object
+  metbPredMat <- apply(metbPredMat, 2, rank)
+  
+  metbSurv <- metbSurvEnt$objects$clinicalSurvData
+  
+  metbLb <- metbLbEnt$objects$object
+
+  ## GENERATE METABRIC 'META'- OR COMMUNITY SCORES
+  indivScore <- NULL
+  metaScore <- NULL
+  for (i in 1:nrow(metbLb)) {
+    indivScore <- c(indivScore, metbLb[i,'Final.Test.Score'])
+    
+    if (i == 1) {
+      metaPrediction <- metbPredMat[ , 2]
+    }
+    else {    
+      metaPrediction <- rowMeans(metbPredMat[ , 2:(i+1)])
+    }
+    performance <- concordance.index(as.numeric(metaPrediction), metbSurv[ , 1], metbSurv[ , 2])
+    metaScore <- c(metaScore, performance$c.index)
+  }
+  
+  metabricDF <- data.frame('mbricIndivScore' = indivScore, 'mbricMetaScore' = metaScore)
+  mMbricDF <- melt(metabricDF)
+  mMbricDF <- data.frame(mMbricDF, 'rank' = rep(1:154, 2))
+  colnames(mMbricDF) <- c('scoreType', 'cci', 'rank')
+  
+  mbricPlot <- ggplot(mMbricDF, aes(rank, cci, colour = scoreType)) +
+    geom_line(size = 4) + 
+    ggtitle('METABRIC: Individual Scores & "MetaScores"\n') +
+    ylab('Concordance Index\n') +
+    xlab('\nLeaderboard Rank')
+  
+  returnList <- list('mbricMetaPlot' = mbricPlot,
+                   'metabricDataFrame' = metabricDF)
+  
+  return(returnList)
+  
+}
---
+++ functions/figureFourB.R
@@ -0,0 +1,66 @@
+## figureFourB.R
+
+## Erich S. Huang
+## Sage Bionetworks
+## erich.huang@sagebase.org
+
+## Adapted from Erhan Biilal's Figure 4B work on
+## analyzing the results of the BCC challenge.
+
+figureFourB <- function(){
+  ## REQUIRE
+  require(synapseClient)
+  require(ggplot2)
+  require(reshape)
+  require(gtools)
+  require(survcomp)
+  
+  ## LOAD NECESSARY DATA OBJECTS
+  osloEnt <- loadEntity('syn1725898') # matrix of predictions 
+  
+  osloSurvEnt <- loadEntity('syn1710257') # survival object
+  
+  osloLbEnt <- loadEntity('syn1739275') # Final osloval leadeboard
+  
+  osloPredMat <- osloEnt$objects$osloPredictions
+  osloPredMat <- data.frame('patientID' = 1:184, osloPredMat)
+  osloPredMat <- apply(osloPredMat, 2, rank)
+  
+  osloSurv <- osloSurvEnt$objects$oslovalSurvData
+  
+  osloLb <- osloLbEnt$objects$object
+  osloLb <- osloLb[1:83, ]
+  
+  ## GENERATE OSLOVAL 'META-' OR COMMUNITY SCORES
+  indivScore <- NULL
+  metaScore <- NULL
+  for (i in 1:83) {
+    indivScore <- c(indivScore, osloLb[i,'CCI'])
+    
+    if (i == 1) {
+      metaPrediction <- osloPredMat[ , 2]
+    }
+    else {    
+      metaPrediction <- rowMeans(osloPredMat[ , 2:(i+1)])
+    }
+    performance <- concordance.index(as.numeric(metaPrediction), osloSurv[ , 1], osloSurv[ , 2])
+    metaScore <- c(metaScore, performance$c.index)
+  }
+  
+  osloDF <- data.frame('osloIndivScore' = indivScore, 'osloMetaScore' = metaScore)
+  mOsloDF <- melt(osloDF)
+  mOsloDF <- data.frame(mOsloDF, rep(1:83, 2))
+  colnames(mOsloDF) <- c('scoreType', 'cci', 'rank')
+  
+  osloPlot <- ggplot(mOsloDF, aes(rank, cci, colour = scoreType)) +
+    geom_line(size = 4) +
+    ggtitle('OsloVal: Individual Scores & "MetaScores"\n') +
+    ylab('Concordance Index\n') +
+    xlab('\nLeaderboard Rank')
+  
+  returnList <- list('osloMetaPlot' = osloPlot,
+                   'oslovalDataFrame' = osloDF)
+  
+  return(returnList)
+  
+}
