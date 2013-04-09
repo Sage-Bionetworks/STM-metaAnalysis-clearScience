ab96ba47537614e94fe2374ccb0ce5f69e8952b7
erichhuang <erich@me.com>
2013-04-09 19:05:23 +0000

    The substance of Figure Three refactor is done.

--- functions/figureThree.R
+++ functions/figureThree.R
@@ -9,14 +9,17 @@ osloSurvEnt <- loadEntity('syn1710257') # survival object
 osloSurv <- osloSurvEnt$objects$oslovalSurvData
 rownames(osloSurv) <- rownames(osloScores)
 
+orderedIDs <- colnames(osloScores)
+
 ## FOR USING THE MCLAPPLY FUNCTION, CONVERT TO LIST
 osloPredList <- mclapply(seq_len(ncol(osloScores)), function(i){osloScores[ , i]})
 names(osloPredList) <- colnames(osloScores)
 
-## CREATE A SAMPLING MATRIX
+## SAMPLE 80% OF THE PREDICTIONS & CALCULATE SCORES 100 TIMES
 set.seed(130409)
 samplePerfMatrix <- matrix(NA, 83, 100)
-for(i in 1:5){
+
+for(i in 1:100){
   iSamplePerfList <- mclapply(osloPredList, function(modelPredVec){
     sampleVec <- sample(1:184, 0.8*nrow(osloScores))
     modelSamp <- modelPredVec[sampleVec]
@@ -29,14 +32,23 @@ for(i in 1:5){
   samplePerfMatrix[ , i] <- iSamplePerfVec
 }
 
+rownames(samplePerfMatrix) <- colnames(osloScores)
+mSampPerfMat <- melt(samplePerfMatrix)
 
+sampleRankMatrix <- apply(samplePerfMatrix, 2, function(x){rank(-x)})
+mSampRankMat <- melt(sampleRankMatrix)
 
+## GENERATE PLOT BY CONCORDANCE INDEX
+ciSampleBoxPlot <- ggplot(mSampPerfMat, aes(X1, value)) + 
+  geom_boxplot() + 
+  geom_jitter(alpha = 0.1) +
+  scale_x_discrete(limits = orderedIDs)
 
+## GENERATE PLOT BY RANK
+rankSampleBoxPlot <- ggplot(mSampRankMat, aes(X1, value)) +
+  geom_boxplot() +
+  geom_jitter(alpha = 0.1) +
+  scale_x_discrete(limits = orderedIDs) +
+  scale_y_reverse()
+  
 
-
-# samplingList <- vector('list', 100)
-# samplingList <- mclapply(samplingList, function(x){rbinom(0.8*nrow(osloScores), 1, 0.5)})
-# 
-# 
-# 
-# foo <- mclapply(sampleMat, function(sampleVec))
