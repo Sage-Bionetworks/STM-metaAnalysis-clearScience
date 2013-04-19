ed5ada6e0c18c1fb17df20875a6382fa84be686e
erichhuang <erich@me.com>
2013-04-19 15:43:29 +0000

    Provenance for clinical table.

--- provenance/provClinicalTable.R
+++ provenance/provClinicalTable.R
@@ -20,4 +20,19 @@ mbricEnt <- getEntity('syn1710260')
 osloClinEnt <- getEntity('syn1710251')
 osloSurvEnt <- getEntity('syn1710257')
 
+## OUTPUT OBJECT
+tableEnt <- getEntity('syn1768409')
 
+## ACTIVITY
+codeLink <- getPermlink(clearSciRepo, 'functions/clinicalTable.R')
+tableAct <- Activity(name = 'Generate Clinical Table',
+                     used = list(
+                       list(url = codeLink, name = basename(codeLink), wasExecuted = T),
+                       list(entity = mbricEnt, wasExecuted = F),
+                       list(entity = osloClinEnt, wasExecuted = F),
+                       list(entity = osloSurvEnt, wasExecuted = F)
+                       ))
+
+tableAct <- createEntity(tableAct)
+generatedBy(tableEnt) <- tableAct
+tableEnt <- storeEntity(tableEnt)
